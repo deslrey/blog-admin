feee5599928c1387186bfe1ba7eab8cf900a41ff
deslrey <2135232170@qq.com>
2025-04-26 17:27:12 +0800

    feat: menuItem component

---
+++ src/components/Table/TableComponent.tsx
@@ -0,0 +1,34 @@
+import { defineComponent } from 'vue'
+
+export const MenuItem = defineComponent({
+    name: 'MenuItem',
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    },
+    setup(props) {
+        return () => {
+            if (props.item.children && props.item.children.length) {
+                return (
+                    <el-sub-menu index={props.item.path} >
+                        {{
+                            title: () => props.item.name,
+                            default: () =>
+                                props.item.children.map((child: any) => (
+                                    <MenuItem item={child} />
+                                ))
+                        }}
+                    </el-sub-menu >
+                )
+            } else {
+                return (
+                    <el-menu-item index={props.item.path} >
+                        {props.item.name}
+                    </el-menu-item >
+                )
+            }
+        }
+    }
+})
