77106d731ea207f0d548d1eeccd6fef9afb30f2f
deslrey <2135232170@qq.com>
2025-04-26 17:49:21 +0800

    feat: add 0.0.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     vueJsxPlugin()
   ],
+
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
@@ -19,6 +20,7 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
     port: 5173,
     proxy: {
       '/deslre': {
