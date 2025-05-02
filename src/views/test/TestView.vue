a6f5f8617d0a833549ed982f9552a1f620c83f50
deslrey <2135232170@qq.com>
2025-05-02 22:47:34 +0800

    style: editor height

--- src/views/test/TestView.vue
+++ src/views/test/TestView.vue
@@ -52,3 +52,31 @@ const onUploadImg = async (files, callback) => {
 };
 
 </script>
+
+<style lang="less" scoped>
+.css-vars(@isDark) {
+    --md-color: if(@isDark, #999, #222);
+    --md-hover-color: if(@isDark, #bbb, #000);
+    --md-bk-color: if(@isDark, #000, #fff);
+    --md-bk-color-outstand: if(@isDark, #333, #f2f2f2);
+    --md-bk-hover-color: if(@isDark, #1b1a1a, #f5f7fa);
+    --md-border-color: if(@isDark, #2d2d2d, #e6e6e6);
+    --md-border-hover-color: if(@isDark, #636262, #b9b9b9);
+    --md-border-active-color: if(@isDark, #777, #999);
+    --md-modal-mask: #00000073;
+    --md-scrollbar-bg-color: if(@isDark, #0f0f0f, #e2e2e2);
+    --md-scrollbar-thumb-color: if(@isDark, #2d2d2d, #0000004d);
+    --md-scrollbar-thumb-hover-color: if(@isDark, #3a3a3a, #00000059);
+    --md-scrollbar-thumb-active-color: if(@isDark, #3a3a3a, #00000061);
+    height: 100%;
+    width: 100%;
+}
+
+.md-editor {
+    .css-vars(false);
+}
+
+.md-editor-dark {
+    .css-vars(true);
+}
+</style>
