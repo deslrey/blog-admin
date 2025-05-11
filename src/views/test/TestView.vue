02f2f1f34e9bf090bf500efb799c737ded1593b6
deslrey <2135232170@qq.com>
2025-05-11 22:29:39 +0800

    feat: implementing paging functionality

--- src/views/test/TestView.vue
+++ src/views/test/TestView.vue
@@ -1,81 +1,92 @@
 <template>
+    <div>
+        <el-table v-if="articleList.length > 0" :data="articleList" style="width: 100%; margin-bottom: 20px" border>
+            <el-table-column prop="id" label="ID" width="80" />
+            <el-table-column prop="title" label="标题" />
+            <el-table-column prop="author" label="作者" />
+            <el-table-column prop="tags" label="标签" />
+            <el-table-column prop="category" label="分类" />
+            <el-table-column prop="createTime" label="创建时间">
+                <template #default="{ row }">
+                    {{ formatDate(row.createTime) }}
+                </template>
+            </el-table-column>
+            <el-table-column prop="wordCount" label="字数" width="80" />
+            <el-table-column prop="readTime" label="阅读时间" width="100" />
+        </el-table>
 
-    <button @click="isEnable = !isEnable">Toggle Form</button>
+        <div class="pagination-wrapper">
+            <el-pagination background layout="total, sizes, prev, pager, next, jumper"
+                :current-page="pagination.pageNum" :page-size="pagination.pageSize" :page-sizes="[10, 15, 20]"
+                :total="pagination.total" @current-change="handlePageChange" @size-change="handleSizeChange" />
+        </div>
 
-    <el-form :model="form" label-width="auto" style="max-width: 600px" v-show="isEnable">
-        <el-form-item label="Activity name">
-            <el-input v-model="form.name" />
-        </el-form-item>
-        <el-form-item label="Activity zone">
-            <el-select v-model="form.region" placeholder="please select your zone">
-                <el-option label="Zone one" value="shanghai" />
-                <el-option label="Zone two" value="beijing" />
-            </el-select>
-        </el-form-item>
-        <el-form-item label="Activity time">
-            <el-col :span="11">
-                <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%" />
-            </el-col>
-            <el-col :span="2" class="text-center">
-                <span class="text-gray-500">-</span>
-            </el-col>
-            <el-col :span="11">
-                <el-time-picker v-model="form.date2" placeholder="Pick a time" style="width: 100%" />
-            </el-col>
-        </el-form-item>
-        <el-form-item label="Instant delivery">
-            <el-switch v-model="form.delivery" />
-        </el-form-item>
-        <el-form-item label="Activity type">
-            <el-checkbox-group v-model="form.type">
-                <el-checkbox value="Online activities" name="type">
-                    Online activities
-                </el-checkbox>
-                <el-checkbox value="Promotion activities" name="type">
-                    Promotion activities
-                </el-checkbox>
-                <el-checkbox value="Offline activities" name="type">
-                    Offline activities
-                </el-checkbox>
-                <el-checkbox value="Simple brand exposure" name="type">
-                    Simple brand exposure
-                </el-checkbox>
-            </el-checkbox-group>
-        </el-form-item>
-        <el-form-item label="Resources">
-            <el-radio-group v-model="form.resource">
-                <el-radio value="Sponsor">Sponsor</el-radio>
-                <el-radio value="Venue">Venue</el-radio>
-            </el-radio-group>
-        </el-form-item>
-        <el-form-item label="Activity form">
-            <el-input v-model="form.desc" type="textarea" />
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="onSubmit">Create</el-button>
-            <el-button>Cancel</el-button>
-        </el-form-item>
-    </el-form>
+    </div>
 </template>
 
-<script lang="ts" setup>
-import { reactive, ref } from 'vue'
+<script setup lang="ts">
+import type { ArticleVO } from '@/types/Article';
+import request from '@/utils/Request';
+import { ref, reactive, computed, onMounted } from 'vue';
 
-// do not use same name with ref
-const form = reactive({
-    name: '',
-    region: '',
-    date1: '',
-    date2: '',
-    delivery: false,
-    type: [],
-    resource: '',
-    desc: '',
-})
+const api = {
+    articleList: '/article/articleList',
+};
 
-const isEnable = ref(false)
+const allArticles = ref<ArticleVO[]>([]); // 全部数据
 
-const onSubmit = () => {
-    console.log('submit!')
-}
+const pagination = reactive({
+    pageNum: 1,
+    pageSize: 10,
+    total: 0,
+});
+
+// 当前页要展示的数据
+const articleList = computed(() => {
+    const start = (pagination.pageNum - 1) * pagination.pageSize;
+    const end = start + pagination.pageSize;
+    return allArticles.value.slice(start, end);
+});
+
+const getDate = async () => {
+    const result = await request.get(api.articleList);
+    console.log('articleList ======> ', result);
+
+    allArticles.value = result.data;
+    pagination.total = result.data.length;
+};
+
+const handlePageChange = (newPage: number) => {
+    pagination.pageNum = newPage;
+};
+
+const handleSizeChange = (newSize: number) => {
+    pagination.pageSize = newSize;
+    pagination.pageNum = 1;
+};
+
+onMounted(() => {
+    getDate();
+});
+
+const formatDate = (date: Date | null): string => {
+    if (!date) return '';
+    const d = new Date(date);
+    return `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(
+        d.getDate()
+    ).padStart(2, '0')}`;
+};
+
 </script>
+
+<style scoped>
+.el-button {
+    margin-bottom: 20px;
+}
+
+.pagination-wrapper {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 10px;
+}
+</style>
