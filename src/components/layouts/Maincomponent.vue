5cc196cf068168e57041603f0e935a1d903d13a8
deslrey <2135232170@qq.com>
2025-05-11 17:39:19 +0800

    style: content area exceeds

--- src/components/layouts/Maincomponent.vue
+++ src/components/layouts/Maincomponent.vue
@@ -14,7 +14,6 @@
     padding: 20px;
     display: flex;
     flex-direction: column;
-    /* 让main本身是弹性布局 */
 }
 
 .content-wrapper {
@@ -22,9 +21,9 @@
     flex: 1;
     display: flex;
     flex-direction: column;
-    padding: 24px;
-    margin: 0 auto;
-    width: 100%;
+    padding: 12px;
+    /* margin: 0 auto; */
+    width: 99%;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.05);
     border-radius: 12px;
     min-height: 0;
